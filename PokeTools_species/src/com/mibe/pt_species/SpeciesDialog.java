9ff95655a116e20a8dafc62650bef96e9b6be441
mibe <mibe@sfc.wide.ad.jp>
2012-11-16 17:36:52 +0900

    種族ダイアログ表示を更新

--- PokeTools_species/src/com/mibe/pt_species/SpeciesDialog.java
+++ PokeTools_species/src/com/mibe/pt_species/SpeciesDialog.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 
 import com.mibe.lib_image.PokeImage;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.widget.TextView;
 
 public class SpeciesDialog extends AlertDialog {
 	
 	private static final float textSize = 0.7f;
+	
+	private Context context = null;
 	
 	private SpeciesBean bean = null;
 	private PokeImage pi = null;
@@ -19,20 +23,55 @@ public class SpeciesDialog extends AlertDialog {
 	private Drawable icon_default = null;
 	
 	private TextView titleView = null;
+	
+	private String sName = null;
 
 	protected SpeciesDialog(Context context, String homeDir) {
+		super(context);		
+		mSetup(context, homeDir, null, null);
+	}
+	
+	protected SpeciesDialog(Context context, String homeDir, Serializable record) {
 		super(context);
+		mSetup(context, homeDir, null, record);
+	}
+	
+	private void mSetup(Context context, String homeDir, String sName, Serializable record){
 		
+		this.context = context;
 		pi = new PokeImage(context, homeDir);
 		icon_default = context.getResources().getDrawable(R.drawable.ic_launcher);
 		titleView = new TextView(context);
+		
+		if(sName != null)this.sName = sName;
+		if(record != null)this.bean = (SpeciesBean) record;
+	}
+	
+	// ダイアログの内容を生成する
+	public void create(){
+		
+		if(bean == null){
+			
+			if(sName == null){
+				
+				// 名前もレコードもない事をエラーとして出力
+			}
+			
+			// 種族名から種族データのBeanを取得する（失敗したらエラー）
+		}
+		
+		// ダイアログのタイトルを設定するする
+		mSetTitle();
+		
+		// ダイアログのビューを設定する
+		mSetView();
 	}
 	
 	public boolean readRecord(Serializable record){
 		
 		if(record == null)return false;
 		
-		bean = (SpeciesBean) record;
+		this.bean = (SpeciesBean) record;
 		
 		mSetTitle();
 		
@@ -59,6 +98,29 @@ public class SpeciesDialog extends AlertDialog {
 		
 		return true;
 		
+	}
+	
+	/**
+	 * ダイアログのビューを設定する
+	 * 内容は図鑑番号，種族値，タイプ，特性，輝石，♂率，体重
+	 * @return
+	 */
+	private boolean mSetView(){
+		
+		if(bean == null)return false;
+		
+		View view = createView();
+		
+		setView(view);
+		
+		return true;
+	}
+	
+	private View createView(){
+		
+		Activity activity = (SpeciesViewActivity)context;
+		
+		return activity.getLayoutInflater().inflate(R.layout.dialog_view, null);
 	}
 
 }
--- PokeTools_species/src/com/mibe/pt_species/SpeciesViewActivity.java
+++ PokeTools_species/src/com/mibe/pt_species/SpeciesViewActivity.java
@@ -2,12 +2,7 @@ package com.mibe.pt_species;
 
 import java.io.Serializable;
 
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.widget.TextView;
-
 import com.mibe.lib_activity.ViewCsvActivity;
-import com.mibe.lib_image.PokeImage;
 
 /**
  * 種族データ一覧のActivityと
@@ -16,8 +11,6 @@ import com.mibe.lib_image.PokeImage;
  *
  */
 public class SpeciesViewActivity extends ViewCsvActivity {
-	
-	private static final float textSize = 0.7f;
 
 	// 表示するファイルのローカルパスを取得する
 	@Override
@@ -32,7 +25,7 @@ public class SpeciesViewActivity extends ViewCsvActivity {
 	}
 	
 	/**
-	 * アイテムが指定された時の処理
+	 * アイテムが指定された時の処理（デフォルトの処理はスキップ）
 	 * record: 指定されたアイテムのレコード
 	 */
 	@Override
@@ -40,34 +33,9 @@ public class SpeciesViewActivity extends ViewCsvActivity {
 		
 		SpeciesBean bean = (SpeciesBean) record;
 		
-		String id = bean.getId();
+		SpeciesDialog dialog = new SpeciesDialog(this, getHomeDir(), bean);
 		
-		super.onListItemClick(record);
-	}
-
-	// ダイアログのタイトルを作成する
-	@Override
-	public View getItemDialogTitle(Serializable record){
-		
-		PokeImage pi = new PokeImage(this, getHomeDir());
-
-		SpeciesBean bean = (SpeciesBean) record;
-
-		String name = bean.getName();
-		String id = bean.getId();
-
-		Drawable icon = pi.getImage("pk", id);
-		if(icon == null) icon = getDefaultIcon();
-		
-		icon = pi.resizeIcon(icon);
-
-		TextView textView = new TextView(this);
-		textView.setText(name);
-
-		textView.setTextSize(icon.getIntrinsicHeight() * textSize);
-
-		textView.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
-
-		return textView;
+		dialog.create();
+		dialog.show();
 	}
 }
